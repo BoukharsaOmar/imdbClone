ea83a5517b1a324403e458d4147a3e380c1fdc73
Boukharsa Omar <omarboukharsa@gmail.com>
2023-11-27 21:58:36 +0100

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+  return <div className="max-w-6xl mx-auto space-y-4">
+    <h1 className="text-2x1 font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie enthusiasts who have come together to create a one-stop destination for all your movie-related needs. Our website is designed to provide you with a comprehensive database of movies from all around the world, along with detailed information about each film, including cast and crew, plot summaries, genres, ratings, and user reviews.
+      </p>
+      <p>
+        Dive into the vast world of cinema with our extensive collection that spans timeless classics, blockbuster hits, indie gems, and international masterpieces. Whether you&#39;re a hardcore film buff or just looking for your next movie night pick, our platform is tailored to cater to your cinematic curiosity.
+      </p>
+      <p>
+        We believe in the magic of movies and their power to entertain, educate, and inspire. That&#39;s why we&#39;ve built features to enhance your experience: personalized recommendations based on your viewing history, curated lists from industry experts, and interactive forums where you can discuss your favorite films with fellow movie lovers.
+      </p>
+      <p>
+        Our mission is to connect people through the shared love of cinema. So, grab some popcorn, explore our site, and let the movie magic begin!
+      </p>
+      
+  </div>;
+}
