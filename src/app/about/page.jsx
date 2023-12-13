2470e02e4c4e3ef74c7d914cbfd617529202bdc5
Boukharsa Omar <omarboukharsa@gmail.com>
2023-12-14 00:27:51 +0100

    create the navbar component

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function About() {
-  return <div className="max-w-6xl mx-auto space-y-4">
+  return <div className="max-w-6xl mx-auto space-y-4 p-4">
     <h1 className="text-2x1 font-medium text-amber-600">About</h1>
       <p>
         Welcome to our movie database website! We are a team of passionate movie enthusiasts who have come together to create a one-stop destination for all your movie-related needs. Our website is designed to provide you with a comprehensive database of movies from all around the world, along with detailed information about each film, including cast and crew, plot summaries, genres, ratings, and user reviews.
